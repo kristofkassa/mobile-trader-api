0863ff9267147bd887efe25f00b4fce0729d09f7
Kristof Kassa <kassakristof@gmail.com>
2020-09-12 12:46:01 +0200

    Initial commit

---
+++ src/main/kotlin/com/trader/mobileAPI/MobileApiApplication.kt
@@ -0,0 +1,11 @@
+package com.trader.mobileAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MobileApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MobileApiApplication>(*args)
+}
